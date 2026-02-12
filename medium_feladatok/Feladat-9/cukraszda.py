549274e90297ec88105f0a00d81d1aa347dd2df7
Ádám <angeloadam@elg-bp.edu.hu>
2026-02-12 17:43:19 +0100

    The class Suti is done, I suppose Pray Peace Protect Commit Enter C'est la vie

--- medium_feladatok/Feladat-9/cukraszda.py
+++ medium_feladatok/Feladat-9/cukraszda.py
@@ -1,5 +1,18 @@
-import os
+import os, random
 os.system("cls")
 
 class Suti():
-    pass
+    def __init__(self, nev:str, tipus:str, ar:int, egyseg:str):
+        self.nev = nev
+        self.tipus = tipus
+        self.ar = ar
+        self.egyseg = egyseg
+        self.eladas = 0
+        self.bevetel = 0
+
+    def EladasGeneralas(self):
+        self.eladas = random.randint(100, 500)
+        return self.eladas
+
+    def BevetelSzamitas(self):
+        self.bevetel = self.eladas * self.ar
