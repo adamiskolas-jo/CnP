b2d3a3d2031bb413e4ceeb67517dddb40f08a707
Ádám <angeloadam@elg-bp.edu.hu>
2026-02-17 09:42:59 +0100

    Advanced with JS Pray Peace Protect Commit Enter C'est la vie

--- hard_feladatok/Feladat-12/cnp.js
+++ hard_feladatok/Feladat-12/cnp.js
@@ -1,16 +1,37 @@
-const username = document.getElementById("username").value
-const password = document.getElementById("password").value
+function login() {
+    const username = document.getElementById("username").value
+    const password = document.getElementById("password").value
+    const tartalom  = document.getElementById("tartalom")
 
+    $.ajax({
+        url: '../../api.php',
+        type: 'POST',
+        data: {
+            username: username,
+            password: password,
+            type: 'login'
+        },
+        success: function (result) {
+            console.log(result);
+            const token = result;
+            $.ajax({
+                url: '../../api.php',
+                type: 'POST',
+                data: {
+                    username: username,
+                    hash: token,
+                    type: "team",
+                    password: "1234565"
+                },
+                success: function (result) {
+                    console.log(result);
+                    eredmeny = JSON.parse(result);
+                    let bekezdes = document.createElement("p");
+                    bekezdes.textContent=eredmeny.user;
+                    tartalom.appendChild(bekezdes);
+                }
+            })
+        }
+    });
+}
 
-$.ajax({
-    url: '../api.php',
-    type: 'POST',
-    data: {
-        username: username,
-        password: password,
-        type: 'login'
-    },
-    success: function (result) {
-        console.log(result);
-    }
-});
