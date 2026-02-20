daa6643f1f456215949bb00809e7e96f02611c0e
Ádám <angeloadam@elg-bp.edu.hu>
2026-02-20 16:26:51 +0100

    Fixes and also made that so now if you delete your cookies it logs you out on interact with profile buttons | JS, HTML Pray Peace Protect Commit Enter C'est la vie

--- js/cnp.js
+++ js/cnp.js
@@ -6,7 +6,7 @@ function login() {
     const username = document.getElementById("username").value
     const password = document.getElementById("password").value
 
-    if (username != "" || password != "") {
+    if (username != "" && password != "") {
         $.ajax({
             url: '../../../api.php',
             type: 'POST',
@@ -31,7 +31,7 @@ function login() {
                         if (result == "No, no Mr. Database nem lenni itthon!" || result == "No, no Mr. User nem lenni itthon!") {
                             setCookie("token", result, -1);
                             setCookie("username", username, -1);
-                            getLoginError.innerHTML = `<p class="text-center">Nem jó felhasználónév és/vagy jelszó!<br>Kérjük próbáld újra!</p>`;
+                            getLoginError.innerHTML = `<p class="text-center">Nem jó felhasználónevet és/vagy jelszót adtál meg!<br>Kérjük próbáld újra!</p>`;
                         }
                         else {
                             window.location.href = "../";
@@ -61,7 +61,17 @@ function login() {
         });
     }
     else {
-        getLoginError.innerHTML = `<p class="text-center">Nem adtál meg felhasználónevet és jelszót sem!<br>Kérjük próbáld újra!</p>`;
+        if (username == "" && password == "") {
+            getLoginError.innerHTML = `<p class="text-center">Nem adtál meg felhasználónevet és jelszót sem!<br>Kérjük próbáld újra!</p>`;
+        }
+        else {
+            if (username == "") {
+                getLoginError.innerHTML = `<p class="text-center">Nem adtál meg felhasználónevet!<br>Kérjük próbáld újra!</p>`;
+            }
+            if (password == "") {
+                getLoginError.innerHTML = `<p class="text-center">Nem adtál meg jelszót!<br>Kérjük próbáld újra!</p>`;
+            }
+        }
     }
 }
 
@@ -84,11 +94,15 @@ document.addEventListener("DOMContentLoaded", function () {
             window.location.href = "/cnp/profile/";
             return;
         }
+        if (getCookie("tokenError") == "True") {
+            getLoginError.innerHTML = `<p class="text-center">Sajnáljuk!<br>Hiba történt a sütikkel!<br>Kérjük próbáld újra a bejelentkezést!</p>`;
+            setCookie("tokenError", "", -1);
+        }
     }
 })
 
 document.addEventListener("DOMContentLoaded", function () {
-    if (loggedIn()) {
+    if (window.location.pathname.endsWith("/cnp/profile/")) {
         $.ajax({
             url: '../../../api.php',
             type: 'POST',
@@ -103,6 +117,7 @@ document.addEventListener("DOMContentLoaded", function () {
                 if (gotAnyError == "No, no Mr. Database nem lenni itthon!" || gotAnyError == "No, no Mr. User nem lenni itthon!") {
                     setCookie("token", "", -1);
                     setCookie("username", "", -1)
+                    setCookie("tokenError", "True", 1)
                     window.location.href = "/cnp/profile/login/";
                 }
                 else {
@@ -115,72 +130,87 @@ document.addEventListener("DOMContentLoaded", function () {
 })
 
 
-function getStat(tipus) {
-    $.ajax({
-        url: '../../../api.php',
-        type: 'POST',
-        data: {
-            username: getCookie("username"),
-            hash: getCookie("token"),
-            type: tipus,
-            password: "CnP_a_best_trust",
-        },
-        success: function (result) {
-            let data = JSON.parse(result)
+function getStat(type) {
+    if (getCookie("tokenError") != "True") {
 
-            if (tipus == "team") {
-                getSec.innerHTML = `<div class="dashboard-card"><div class="dashboard-header"><h2>Csapat adatok</h2><p>${data.user}</p></div><div class="dashboard-grid"><div class="stat-box"><span>Csapat</span><strong>${data.csapat}</strong></div><div class="stat-box"><span>Csoport</span><strong>${data.csoport}</strong></div><div class="stat-box"><span>Pozíció</span><strong>${data.pozíció}</strong></div><div class="stat-box"><span>Pont</span><strong>${data.pont}</strong></div><div class="stat-box"><span>Osztály</span><strong>${data.osztály}</strong></div></div></div>`;
-            }
-
-            /* Old approach
-                let h3 = document.createElement("h3");
-                h3.textContent = data.csapat;
-                getSec.appendChild(h3);   */
-
-
-            if (tipus == "score") {
-                getSec.innerHTML = `<div class="row"><div class="col-12"><h3 class="text-white">Csapatok összesített adatai:</h3><p class="text-white">Rendezés: pontszám szerinti</p><div class="table-responsive"><table class="table table-padding table-striped table-hover table-dark"><thead><tr><th>Helyezés</th><th>Csapatneve</th><th>Pontszám</th><th>Osztály</th></tr></thead><tbody id="tableLeaderboardBody"></tbody></table>`
-                let tLBbody = document.getElementById("tableLeaderboardBody");
-                data.forEach((task, index) => {
-                    tLBbody.innerHTML += `<tr><td>${index + 1}.</td><td>${task.team}</td><td>${task.pont}</td><td>${task.osztály}</tr>`;
+        $.ajax({
+            url: '../../../api.php',
+            type: 'POST',
+            data: {
+                username: getCookie("username"),
+                hash: getCookie("token"),
+                type: type,
+                password: "CnP_a_best_trust",
+            },
+            success: function (result) {
+                if (result == "No, no Mr. User nem lenni itthon!" || result == "No, no Mr. Database nem lenni itthon!") {
+                    setCookie("tokenError", "True", 1);
+                    setCookie("token", "", -1);
+                    setCookie("username", "", -1);
+                    window.location.href = "./login/";
                 }
-                )
+                let data = JSON.parse(result)
 
-            }
-            if (tipus == "task") {
-                getSec.innerHTML = `<div class="col-12"><h3 class="text-white">Felvett feladatok listája:</h3>
+                if (type == "team") {
+                    getSec.innerHTML = `<div class="dashboard-card"><div class="dashboard-header"><h2>Csapat adatok</h2><p>${data.user}</p></div><div class="dashboard-grid"><div class="stat-box"><span>Csapat</span><strong>${data.csapat}</strong></div><div class="stat-box"><span>Csoport</span><strong>${data.csoport}</strong></div><div class="stat-box"><span>Pozíció</span><strong>${data.pozíció}</strong></div><div class="stat-box"><span>Pont</span><strong>${data.pont}</strong></div><div class="stat-box"><span>Osztály</span><strong>${data.osztály}</strong></div></div></div>`;
+                }
+
+                /* Old approach
+                    let h3 = document.createElement("h3");
+                    h3.textContent = data.csapat;
+                    getSec.appendChild(h3);   */
+
+
+                if (type == "score") {
+                    getSec.innerHTML = `<div class="row"><div class="col-12"><h3 class="text-white">Csapatok összesített adatai:</h3><p class="text-white">Rendezés: pontszám szerinti</p><div class="table-responsive"><table class="table table-padding table-striped table-hover table-dark"><thead><tr><th>Helyezés</th><th>Csapatneve</th><th>Pontszám</th><th>Osztály</th></tr></thead><tbody id="tableLeaderboardBody"></tbody></table>`
+                    let tLBbody = document.getElementById("tableLeaderboardBody");
+                    data.forEach((score, index) => {
+                        if (score.team != "Tesztike") {
+                            tLBbody.innerHTML += `<tr><td>${index + 1}.</td><td>${score.team}</td><td>${score.pont}</td><td>${score.osztály}</tr>`;
+                        }
+                    }
+                    )
+
+                }
+                if (type == "task") {
+                    getSec.innerHTML = `<div class="col-12"><h3 class="text-white">Felvett feladatok listája:</h3>
                 <p class="text-white">Rendezés: felvétel szerinti</p><div class="table-responsive"><table class="table table-padding text-center table-striped table-hover table-dark"><thead><tr><th>Sorszám</th><th>Cím</th><th>Nehézség</th><th>Megszerezhető pontszám</th><th>Lejárat</th><th>Rövid leírás</th><th>Státusz</th></tr></thead><tbody id="tableActiveTasksBody"></tbody></table>`;
-                let tATbody = document.getElementById("tableActiveTasksBody");
+                    let tATbody = document.getElementById("tableActiveTasksBody");
 
-                data.forEach((task, index) => {
-                    tATbody.innerHTML += `<tr><td>${index + 1}.</td><td>${task.cim}</td><td>${task.lvl}</td><td>${task.megszerezheto_pont}</td><td>${task.lejarat}</td><td>${task.rövid_leiras}</td><td>${task.state}</td></tr>`;
+                    data.forEach((task, index) => {
+                        tATbody.innerHTML += `<tr><td>${index + 1}.</td><td>${task.cim}</td><td>${task.lvl}</td><td>${task.megszerezheto_pont}</td><td>${task.lejarat}</td><td>${task.rövid_leiras}</td><td>${task.state}</td></tr>`;
+                    }
+                    )
                 }
-                )
-            }
-            if (tipus == "able_task") {
-                getSec.innerHTML = `<div class="col-12"><h3 class="text-white">Elérhető feladatok listája:</h3>
+                if (type == "able_task") {
+                    getSec.innerHTML = `<div class="col-12"><h3 class="text-white">Elérhető feladatok listája:</h3>
                 <p class="text-white">Rendezés: hozzáadás szerinti</p><div class="table-responsive"><table class="table text-center table-padding table-striped table-hover table-dark"><thead><tr><th>Sorszám</th><th>Cím</th><th>Nehézség</th><th>Megszerezhető pontszám</th><th>Lejárat</th><th>Rövid leírás</th></tr></thead><tbody id="tableAvailableTasksBody"></tbody></table>`;
-                let tATbody = document.getElementById("tableAvailableTasksBody");
+                    let tATbody = document.getElementById("tableAvailableTasksBody");
 
-                data.forEach((task, index) => {
-                    tATbody.innerHTML += `<tr><td>${index + 1}.</td><td>${task.cim}</td><td>${task.lvl}</td><td>${task.megszerezheto_pont}</td><td>${task.lejarat}</td><td>${task.rövid_leiras}</td></tr>`;
+                    data.forEach((task, index) => {
+                        tATbody.innerHTML += `<tr><td>${index + 1}.</td><td>${task.cim}</td><td>${task.lvl}</td><td>${task.megszerezheto_pont}</td><td>${task.lejarat}</td><td>${task.rövid_leiras}</td></tr>`;
+                    }
+                    )
                 }
-                )
+
             }
-        }
-    });
+        });
+    }
+    else {
+        setCookie("token", "", -1);
+        setCookie("username", "", -1);
+        window.location.href = "./login/";
+    }
 }
 
 
 function logout() {
-    hash = getCookie("token")
+    /* hash = getCookie("token")
     username = getCookie("username")
     user = username
-    token = hash
-    setCookie("token", hash, -1);
-    setCookie("username", username, -1);
-    setCookie("user", username, -1);
-    setCookie("hash", hash, -1);
+    token = hash */
+    setCookie("token", "", -1);
+    setCookie("username", "", -1);
     window.location.href = "../";
 }
 
