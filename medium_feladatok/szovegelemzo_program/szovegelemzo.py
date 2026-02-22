da35f84e841a0195053613659e539912e53855dc
kovaron <kovacsaron@elg-bp.edu.hu>
2026-02-22 15:08:09 +0100

    karakterek száma, szavak száma, magánhangzók száma

--- medium_feladatok/szovegelemzo_program/szovegelemzo.py
+++ medium_feladatok/szovegelemzo_program/szovegelemzo.py
@@ -2,7 +2,16 @@ import os
 os.system("cls")
 
 szoveg = input("Adjon meg egy tetzőleges szöveget: ")
+szovegLista = szoveg.split()
+karakterekSzama = 0
+maganhanzokSzama = 0
+szavakSzama = len(szovegLista)
 
-maganhangzok = "aeouöüóőúáűeé"
-maganhangzolista = list(maganhangzok)
+for betu in szoveg:
+    karakterekSzama += 1
+    if betu in ["a", "á", "e", "é", "i", "í", "o", "ó", "ö", "ő", "ü", "ű", "u", "ú"]:
+        maganhanzokSzama += 1
 
+print(f"Karakterek száma: {karakterekSzama}")
+print(f"Szavak száma: {szavakSzama}")
+print(f"Magánhangzók száma: {maganhanzokSzama}")
