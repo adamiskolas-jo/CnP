c424812c854e83542a6b0b7632b35ab545f272e7
Ádám <angeloadam@elg-bp.edu.hu>
2026-02-22 18:54:19 +0100

    Character generation mostly done Pray Peace Protect Commit Enter C'est la vie

--- brutalis_feladatok/feladat-11/harcijatek.py
+++ brutalis_feladatok/feladat-11/harcijatek.py
@@ -1,9 +1,8 @@
 import os, random
-os.system("cls")
+def cls():
+    os.system("cls")
 
-felhasználó = input("adjon meg egy nevet:")
-
-kivalasztottKaszt = input("Add meg a kasztod :")
+cls()
 
 def dobas(hanyszorGeneralja:int, meddig:int, hozzaadottSzam:int=0):
     eredmeny = 0
@@ -11,12 +10,12 @@ def dobas(hanyszorGeneralja:int, meddig:int, hozzaadottSzam:int=0):
         eredmeny += random.randint(1, meddig)
     return eredmeny + hozzaadottSzam
 
-class character():
-    def __init__(self, nev, kaszt, fegyver):
+class Karakter():
+    def __init__(self, nev:str, kaszt:str, fegyver:str):
         self.nev = nev
         self.kaszt = kaszt
         self.fegyver = fegyver
-        self.hp = 25
+        self.hp = 25    
 
         if kaszt == "harcos":
             self.ero = dobas(1, 10, 10)
@@ -33,39 +32,147 @@ class character():
             self.gyorsasag = dobas(3,6)
             self.ugyesseg = dobas(1,10,10)
 
+        # Fegyverek
         
         if fegyver == "kard":
             self.fegyverDmg = dobas(1,6,3)
             self.fegyverSpeed = 6
             self.fegyverDefense = 8
             self.fegyverAttack = 6
+            self.fegyverKardBlacklist = ""
  
         elif fegyver == "tőr":
             self.fegyverDmg = dobas(1,6)
             self.fegyverSpeed = 10
             self.fegyverDefense = 3
             self.fegyverAttack = 10
-            # Harcos NEM használhatja
+            self.fegyverTorBlacklist = "harcos"
         
         elif fegyver == "bot":
             self.fegyverDmg = dobas(1,4)
             self.fegyverSpeed = 8
             self.fegyverDefense = 10
             self.fegyverAttack = 8
-            # CSAK a pap használhatja
+            self.fegyverBotWhitelist = "pap"
  
         elif fegyver == "pallos":
             self.fegyverDmg = dobas(2,6)
             self.fegyverSpeed = 1
             self.fegyverDefense = 1
             self.fegyverAttack = 4
-            # CSAK A HARCOS
+            self.fegyverPallosWhitelist = "harcos"
         
         elif fegyver == "buzogány":
             self.fegyverDmg = dobas(2,4,2)
             self.fegyverSpeed = 4
             self.fegyverDefense = 5
             self.fegyverAttack = 4
-            # a Tolvaj NEM használhatja
+            self.fegyverBuzoganyBlacklist = "tolvaj"
         
+        self.tamadas = max(0, self.ero - 10) + self.fegyverAttack 
+        self.vedekezes = max(0, self.ugyesseg - 10) + self.fegyverDefense  
+        self.kezdemenyezes = max(0, self.gyorsasag - 10) + self.fegyverSpeed
+       
 
+def karakterLetrehozas():
+    cls()
+    print(">>>> Karakter Létrehozás <<<<")
+    kivalasztottNev = input("Adj meg egy nevet:")
+    while True:
+        print(" Válassz egy kasztot!\n  Választható kasztok:")
+        print("  1. Harcos\n  2. Tolvaj\n  3. Pap")
+        kivalasztottKaszt = int(input("Add meg a kasztod (1-3):   "))
+        match kivalasztottKaszt:
+            case 1:
+                kivalasztottKaszt = "harcos"
+                break
+            case 2:
+                kivalasztottKaszt = "tolvaj"
+                break
+            case 3:
+                kivalasztottKaszt = "pap"
+                break
+            case _:
+                input("Helytelen kaszt!\nEnter az újrapróbáláshoz")
+                cls()
+
+    while True:
+        cls()
+        print(" Válassz egy fegyvert!\n  Választható fegyverek:")
+        print("  1. Kard")
+
+        if kivalasztottKaszt == "harcos":
+            print("  2. Tőr | Harcos Exklúzív")
+            print("  3. Pallos | Harcos Exklúzív")
+            print("  4. Buzogány")
+            kivalasztottFegyver = int(input("Add meg a fegyvered (1-4):   "))
+            match kivalasztottFegyver:
+                case 1:
+                    kivalasztottFegyver = "kard"
+                    break
+                case 2:
+                    kivalasztottFegyver = "tőr"
+                    break
+                case 3:
+                    kivalasztottFegyver = "pallos"
+                    break
+                case 4:
+                    kivalasztottFegyver = "buzogány"
+                    break
+                case _:
+                    input("Helytelen választás!\nEnter az újrapróbáláshoz!")
+
+
+        elif kivalasztottKaszt == "tolvaj":
+            kivalasztottFegyver = int(input("Add meg a fegyvered (1-1):   ")) # egyenlőre neki csak kardja van :c
+            match kivalasztottFegyver:
+                case 1:
+                    kivalasztottFegyver = "kard"
+                    break
+                case _:
+                    input("Helytelen !\nEnter az újrapróbáláshoz!")
+
+        elif kivalasztottKaszt == "pap":
+            print("  2. Bot | Pap Exklúzív")
+            print("  3. Buzogány")
+            kivalasztottFegyver = int(input("Add meg a fegyvered (1-3):   "))
+            match kivalasztottFegyver:
+                case 1:
+                    kivalasztottFegyver = "kard"
+                    break
+                case 2:
+                    kivalasztottFegyver = "bot"
+                    break
+                case 3:
+                    kivalasztottFegyver = "buzogány"
+                    break
+                case _:
+                    input("Helytelen !\nEnter az újrapróbáláshoz!")
+
+    letrehozottKarakter = Karakter(kivalasztottNev, kivalasztottKaszt, kivalasztottFegyver)
+    cls()
+    cls()
+    print(">>> Karakter Létrehozva <<<")
+    print("Alap statisztika:")
+    print(f"  Név: {letrehozottKarakter.nev}")
+    print(f"  Kaszt: {letrehozottKarakter.kaszt}")
+    print(f"  Fegyver: {letrehozottKarakter.fegyver}")
+    print("\nKarakter statisztika:")
+    print(f"  Életerő: {letrehozottKarakter.hp}")
+    print(f"  Erő: {letrehozottKarakter.ero}")
+    print(f"  Gyorsaság: {letrehozottKarakter.gyorsasag}")
+    print(f"  Ügyesség: {letrehozottKarakter.ugyesseg}")
+    print(f"  Támadás: {letrehozottKarakter.tamadas}")
+    print(f"  Védekezés: {letrehozottKarakter.vedekezes}")
+    print(f"  Kezdeményezés: {letrehozottKarakter.kezdemenyezes}")
+    input("\nFegyver statisztika (ENTER):   ")
+    print(f"  Fegyver típusa: {letrehozottKarakter.fegyver}")
+    print(f"  Fegyver Sebzése: {letrehozottKarakter.fegyverDmg}")
+    print(f"  Fegyver Védekezése: {letrehozottKarakter.fegyverDefense}")
+    print(f"  Fegyver Gyorsasága: {letrehozottKarakter.fegyverSpeed}")
+    print(f"  Fegyver Támadása: {letrehozottKarakter.fegyverAttack}")
+    input("\nENTER a folytatáshoz!   ")
+
+    return letrehozottKarakter
+
+karakter1 = karakterLetrehozas()
